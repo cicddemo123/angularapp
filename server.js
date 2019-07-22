b0baddb0645d2a320c9b36ddb868e35f5b7bd41f
Sasi <sasi.p@vvdntech.in>
2019-07-22 14:13:32 +0530

    5

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname+'/dist'));
+app.use(express.static(__dirname+'/dist/demo-deploy'));
 
 app.listen(process.env.PORT||8080);
 
 
 //Path Location Strategy
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/demo-deploy/index.html'));
 });
 
 console.log('Console Listening');
